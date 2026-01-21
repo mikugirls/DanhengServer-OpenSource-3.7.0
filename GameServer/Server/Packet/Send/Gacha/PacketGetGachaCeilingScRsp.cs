cdf67d98e9e685b0d1a870b2a81a5c93fa9f685c
zb1240610539 <zb1240610539@126.com>
2026-01-21 16:21:07 +0800

    Create PacketGetGachaCeilingScRsp.cs

---
+++ GameServer/Server/Packet/Send/Gacha/PacketGetGachaCeilingScRsp.cs
@@ -0,0 +1,9 @@
+namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Gacha;
+
+public class PacketGetGachaCeilingScRsp : BasePacket
+{
+    public PacketGetGachaCeilingScRsp(GetGachaCeilingScRsp proto) : base(CmdIds.GetGachaCeilingScRsp)
+    {
+        SetData(proto);
+    }
+}
