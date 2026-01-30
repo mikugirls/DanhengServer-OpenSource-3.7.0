198ad0418addfe012bb2ae9b437c7a967bdc031b
zb1240610539 <zb1240610539@126.com>
2026-01-30 13:54:12 +0800

    Create PacketCancelExpeditionScRsp.cs

---
+++ GameServer/Server/Packet/Send/Expedition/PacketCancelExpeditionScRsp.cs
@@ -0,0 +1,23 @@
+using EggLink.DanhengServer.Kcp;
+using EggLink.DanhengServer.Proto;
+
+namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Expedition;
+
+public class PacketCancelExpeditionScRsp : BasePacket
+{
+    public PacketCancelExpeditionScRsp(uint expeditionId) : base(CmdIds.CancelExpeditionScRsp)
+    {
+        var proto = new CancelExpeditionScRsp
+        {
+            Retcode = 0,
+            ExpeditionId = expeditionId,
+            // 中途取消通常没有奖励，初始化为空对象
+            Reward = new ItemList(),
+            ExtraReward = new ItemList(),
+            // 结束时间设为当前服务器时间
+            FinishTime = Extensions.GetUnixSec()
+        };
+
+        SetData(proto);
+    }
+}
