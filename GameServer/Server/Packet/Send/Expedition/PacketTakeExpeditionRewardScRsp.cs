dc0486e28f261a33ae923e739a897265b580deb8
zb1240610539 <zb1240610539@126.com>
2026-01-30 14:19:15 +0800

    Update PacketTakeExpeditionRewardScRsp.cs

--- GameServer/Server/Packet/Send/Expedition/PacketTakeExpeditionRewardScRsp.cs
+++ GameServer/Server/Packet/Send/Expedition/PacketTakeExpeditionRewardScRsp.cs
@@ -5,19 +5,18 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Expedition;
 
 public class PacketTakeExpeditionRewardScRsp : BasePacket
 {
-    // 构造函数接收 派遣ID 和 奖励列表
-   public class PacketTakeExpeditionRewardScRsp : BasePacket
-{
-    public PacketTakeExpeditionRewardScRsp(uint expeditionId, ItemList reward, ItemList extraReward) : base(CmdIds.TakeExpeditionRewardScRsp)
+    // 构造函数：必须没有返回类型（如 void/int），名称必须与类名完全一致
+    public PacketTakeExpeditionRewardScRsp(uint expeditionId, ItemList reward, ItemList extraReward) 
+        : base(CmdIds.TakeExpeditionRewardScRsp)
     {
         var proto = new TakeExpeditionRewardScRsp
         {
             Retcode = 0,
             ExpeditionId = expeditionId,
             Reward = reward,
-            ExtraReward = extraReward // 填充额外奖励
+            ExtraReward = extraReward // 这里存放角色加成带来的额外奖励
         };
+
         SetData(proto);
     }
-}
 }
