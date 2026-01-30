9ba686472ce3f96202b1671912acfc303c424368
zb1240610539 <zb1240610539@126.com>
2026-01-30 10:37:12 +0800

    Update PacketGetExpeditionDataScRsp.cs

--- GameServer/Server/Packet/Send/Expedition/PacketGetExpeditionDataScRsp.cs
+++ GameServer/Server/Packet/Send/Expedition/PacketGetExpeditionDataScRsp.cs
@@ -1,23 +1,23 @@
+using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 using EggLink.DanhengServer.GameServer.Game.Player;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Expedition;
 
-public class PacketGetExpeditionDataScRsp : ScPacket
+public class PacketGetExpeditionDataScRsp : BasePacket
 {
     public PacketGetExpeditionDataScRsp(PlayerInstance player) : base(CmdIds.GetExpeditionDataScRsp)
     {
-        var rsp = new GetExpeditionDataScRsp
+        var proto = new GetExpeditionDataScRsp
         {
             Retcode = 0
         };
 
-        // 从之前编写的数据库模型中提取数据并填入协议
-        rsp.ExpeditionList.AddRange(player.ExpeditionManager.Data.ToProto());
+        [cite_start]// 从 ExpeditionManager 的数据库模型中提取数据并填充 [cite: 3]
+        // 这里使用了我们之前在 ExpeditionData 中定义的 ToProto() 转换方法
+        proto.ExpeditionList.AddRange(player.ExpeditionManager.Data.ToProto());
 
-        // 如果需要，可以同步当前已解锁的槽位数量
-        // rsp.UnlockedTeamCount = (uint)player.ExpeditionManager.GetUnlockedExpeditionSlots();
-
-        this.Body = rsp;
+        // 设置协议数据
+        SetData(proto);
     }
 }
