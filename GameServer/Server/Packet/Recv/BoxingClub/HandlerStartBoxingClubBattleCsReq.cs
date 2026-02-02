c961c755483c6f3f8c6314c66db4774988fe2340
zb1240610539 <zb1240610539@126.com>
2026-02-02 15:10:54 +0800

    Update HandlerStartBoxingClubBattleCsReq.cs

--- GameServer/Server/Packet/Recv/BoxingClub/HandlerStartBoxingClubBattleCsReq.cs
+++ GameServer/Server/Packet/Recv/BoxingClub/HandlerStartBoxingClubBattleCsReq.cs
@@ -12,6 +12,19 @@ public class HandlerStartBoxingClubBattleCsReq : Handler
 {
     public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
     {
-       
+        var req = StartBoxingClubBattleCsReq.Parser.ParseFrom(data);
+        var player = connection.Player!;
+
+        // 逻辑全部下沉到 Manager，直接返回构建好的战斗信息
+        var sceneBattleInfo = player.BoxingClubManager.StartBattle(req.ChallengeId);
+
+        if (sceneBattleInfo != null)
+        {
+            await connection.SendPacket(new PacketStartBoxingClubBattleScRsp((uint)Retcode.RetSucc, sceneBattleInfo));
+        }
+        else
+        {
+            await connection.SendPacket(new PacketStartBoxingClubBattleScRsp((uint)Retcode.RetBoxingClubChallengeNotOpen));
+        }
     }
 }
