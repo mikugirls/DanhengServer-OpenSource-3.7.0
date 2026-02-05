ac88be8e701f371d09a3d50078fde4c9705fd26e
zb1240610539 <zb1240610539@126.com>
2026-02-05 16:13:22 +0800

    Update PacketGetShopListScRsp.cs

--- GameServer/Server/Packet/Send/Shop/PacketGetShopListScRsp.cs
+++ GameServer/Server/Packet/Send/Shop/PacketGetShopListScRsp.cs
@@ -3,26 +3,36 @@ using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Shop;
-
 public class PacketGetShopListScRsp : BasePacket
 {
-    public PacketGetShopListScRsp(uint shopType) : base(CmdIds.GetShopListScRsp)
+    public PacketGetShopListScRsp(PlayerInstance player, uint shopType) : base(CmdIds.GetShopListScRsp)
     {
-        var proto = new GetShopListScRsp
-        {
-            ShopType = shopType
-        };
+        var proto = new GetShopListScRsp { ShopType = shopType, Retcode = 0 };
 
         foreach (var item in GameData.ShopConfigData.Values)
+        {
             if (item.ShopType == shopType && item.Goods.Count > 0)
-                proto.ShopList.Add(new Proto.Shop
+            {
+                int shopId = item.ShopID;
+                var shopProto = new Proto.Shop
                 {
-                    ShopId = (uint)item.ShopID,
+                    ShopId = (uint)shopId,
                     CityLevel = 1,
                     EndTime = uint.MaxValue,
                     GoodsList = { item.Goods.Where(x => x.ItemID != 0).Select(g => g.ToProto()) }
-                });
+                };
 
+                // --- 调用 ShopService 处理数据 ---
+                if (GameData.CityShopConfigData.ContainsKey(shopId))
+                {
+                    shopProto.CityLevel = player.ShopService!.CalculateCityLevel(shopId);
+                    shopProto.CityExp = player.CityShopData!.GetExp(shopId);
+                    shopProto.CityTakenLevelReward = player.CityShopData!.GetRewardMask(shopId);
+                }
+
+                proto.ShopList.Add(shopProto);
+            }
+        }
         SetData(proto);
     }
 }
