b9d1e63c5d1149078c8524f4d31eb8c5907e6203
zb1240610539 <zb1240610539@126.com>
2026-02-09 14:53:20 +0800

    Update PacketTakeCityShopRewardScRsp.cs

--- GameServer/Server/Packet/Send/Shop/PacketTakeCityShopRewardScRsp.cs
+++ GameServer/Server/Packet/Send/Shop/PacketTakeCityShopRewardScRsp.cs
@@ -2,11 +2,21 @@ using EggLink.DanhengServer.Proto;
 using EggLink.DanhengServer.Kcp; // 修复 BasePacket 找不到的问题
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Shop;
-
 public class PacketTakeCityShopRewardScRsp : BasePacket
 {
-    public PacketTakeCityShopRewardScRsp(TakeCityShopRewardScRsp rspData) : base(CmdIds.TakeCityShopRewardScRsp)
+    // 修改构造函数，支持直接传入 4 个参数
+    public PacketTakeCityShopRewardScRsp(uint retcode, uint level, uint shopId, ItemList? reward = null) 
+        : base(CmdIds.TakeCityShopRewardScRsp)
     {
-        SetData(rspData);
+        var proto = new TakeCityShopRewardScRsp
+        {
+            Retcode = retcode,
+            Level = level,
+            ShopId = shopId,
+            Reward = reward ?? new ItemList() 
+        };
+
+        SetData(proto);
     }
 }
+
