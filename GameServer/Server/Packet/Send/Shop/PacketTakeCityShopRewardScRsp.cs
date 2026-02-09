7b005d90d408d419f3b411438c88f8173956fa66
zb1240610539 <zb1240610539@126.com>
2026-02-09 14:44:10 +0800

    Update PacketTakeCityShopRewardScRsp.cs

--- GameServer/Server/Packet/Send/Shop/PacketTakeCityShopRewardScRsp.cs
+++ GameServer/Server/Packet/Send/Shop/PacketTakeCityShopRewardScRsp.cs
@@ -1,27 +1,12 @@
 using EggLink.DanhengServer.Proto;
-using EggLink.DanhengServer.Kcp; // 必须引用这个命名空间来找到 BasePacket
+using EggLink.DanhengServer.Kcp; // 修复 BasePacket 找不到的问题
+
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Shop;
 
 public class PacketTakeCityShopRewardScRsp : BasePacket
 {
-    /// <summary>
-    /// 构造城市商店领奖响应包 (CmdId: 1586)
-    /// </summary>
-    /// <param name="retcode">错误码 (来自 Retcode.proto)</param>
-    /// <param name="level">领取的等级</param>
-    /// <param name="shopId">商店ID</param>
-    /// <param name="reward">获得的道具列表</param>
-    public PacketTakeCityShopRewardScRsp(uint retcode, uint level, uint shopId, ItemList reward = null) : base(CmdIds.TakeCityShopRewardScRsp)
+    public PacketTakeCityShopRewardScRsp(TakeCityShopRewardScRsp rspData) : base(CmdIds.TakeCityShopRewardScRsp)
     {
-        // 这里的字段赋值会自动匹配你刚才修复的 Tag 3, 10, 12, 15
-        var proto = new TakeCityShopRewardScRsp
-        {
-            Retcode = retcode,
-            Level = level,
-            ShopId = shopId,
-            Reward = reward ?? new ItemList() // 如果没有奖励，传空列表防止客户端显示异常
-        };
-
-        SetData(proto);
+        SetData(rspData);
     }
 }
