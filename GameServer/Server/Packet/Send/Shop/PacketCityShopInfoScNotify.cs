bc213360b9617e15e51fdf2983ce422db8c768cf
zb1240610539 <zb1240610539@126.com>
2026-02-10 09:28:26 +0800

    Update PacketCityShopInfoScNotify.cs

--- GameServer/Server/Packet/Send/Shop/PacketCityShopInfoScNotify.cs
+++ GameServer/Server/Packet/Send/Shop/PacketCityShopInfoScNotify.cs
@@ -7,24 +7,20 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Shop;
 public class PacketCityShopInfoScNotify : BasePacket
 {
     public PacketCityShopInfoScNotify(PlayerInstance player, int shopId) : base(CmdIds.CityShopInfoScNotify)
+{
+    uint currentExp = player.CityShopData?.GetExp(shopId) ?? 0;
+    uint physicalMax = player.ShopService!.GetPhysicalMaxLevel(shopId, currentExp);
+    ulong takenMask = player.CityShopData?.GetRewardMask(shopId) ?? 0;
+
+    var notify = new CityShopInfoScNotify
     {
-        // 1. 获取当前总经验
-        uint currentExp = player.CityShopData?.GetExp(shopId) ?? 0;
-        
-        // 2. 调用 ShopService 计算当前等级
-        uint currentLevel = player.ShopService!.CalculateCityLevel(shopId);
+        ShopId = (uint)shopId,
+        Exp = currentExp,
+        // 维持 Level = physicalMax + 1 方案，确保 UI 判定所有达标等级为“已完成”
+        Level = physicalMax + 1, 
+        TakenLevelReward = takenMask // 此时领了1级，这里发的就是 2 (二进制 10)
+    };
 
-        // 3. 获取已领取奖励的位掩码 (直接使用 ulong)
-        ulong takenMask = player.CityShopData?.GetRewardMask(shopId) ?? 0;
-
-        var notify = new CityShopInfoScNotify
-        {
-            ShopId = (uint)shopId,
-            Exp = currentExp,
-            Level = currentLevel,
-            TakenLevelReward = takenMask // 对应 Proto 中的第 15 号字段 (ulong)
-        };
-
-        SetData(notify);
-    }
+    SetData(notify);
+}
 }
